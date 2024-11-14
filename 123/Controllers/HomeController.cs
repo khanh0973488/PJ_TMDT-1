b357153f1cba3540dcffd413de47cb8fd9240cb8
hungubqn03 <anhung03102003@gmail.com>
2024-11-14 10:58:04 +0700

    Thêm danh mục

--- 123/Controllers/HomeController.cs
+++ 123/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace _123.Controllers
         {
             return View();
         }
-
+        public IActionResult DanhMuc()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
